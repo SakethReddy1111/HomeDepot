e8ce006f63dcbb060da2efcff9948cf16619df15
saketh reddy <sakethr007@gmail.com>
2022-01-19 16:12:05 +0530

    completed the footer

---
+++ components/footer.js
@@ -0,0 +1,171 @@
+let footerFunc = () => {
+  return `<div id="class312">
+    <h3>How doers get more done <sup>TM</sup></h3>
+    <p>
+      Need Help? please call us at :
+      <a href="">1-800-HOME-DEPOT (1-800-466-3337)</a>
+    </p>
+  </div>
+  <div id="id421">
+    <div class="class230">
+      <h3>Customer Service</h3>
+      <p>Check Order Status</p>
+      <p>Pay Your Credit Card</p>
+      <p>Order Cancellation</p>
+      <p>Returns</p>
+      <p>Shipping & Delivery</p>
+      <p>Products & Delivery</p>
+      <p>Help & FAQs</p>
+      <p>Privacy & Security Center</p>
+    </div>
+    <div class="class230">
+      <h3>Resources</h3>
+      <p>Special & offers</p>
+      <p>military discount benefit</p>
+      <p>DIY projects & ideas</p>
+      <p>truck & tool rental</p>
+      <p>Installation & Services</p>
+      <p>moving supplies & rental</p>
+      <p>protection plans</p>
+      <p>rebate center</p>
+      <p>Gift cards</p>
+      <p>catalog</p>
+      <p>subscriptions</p>
+    </div>
+    <div class="class230">
+      <h3>About Us</h3>
+      <p>carrers</p>
+      <p>corporate information</p>
+      <p>digital newsroom</p>
+      <p>home depot foundation</p>
+      <p>investor relations</p>
+      <p>government customers</p>
+      <p>suppliers & providers</p>
+      <p>affilate program</p>
+      <p>eco options</p>
+      <p>corporate responsibility</p>
+      <p>home depot licensing information</p>
+    </div>
+    <div>
+      <div id="id8901">
+        <div id="id7601">
+          <i class="fab fa-cc-visa"></i>
+          <p>Special Financing Available everyday*</p>
+        </div>
+        <a href="">Pay & Manage your Card</a>
+        <br />
+        <a href="">Credit Offers</a>
+      </div>
+      <p class="class025178">
+        Get $5 off when you sign up for emails with savings and tips
+      </p>
+      <div id="id66622">
+        <input
+          type="text"
+          id="id666221"
+          placeholder="Enter Email Address"
+        />
+        <input type="submit" id="id666222" value="GO" />
+      </div>
+    </div>
+  </div>
+  <div id="id500">
+    <h3>Shop Our Brands</h3>
+    <div id="id603">
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/company-store.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/blinds-dot-com.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/home-decorator.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/hampton-bay.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/husky.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/lifeproof.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/glacier-bay.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/everbilt.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/style-well.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/hdx.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/vigoro.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/traffic-master.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/defiant.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/eco-smart.png"
+        alt=""
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/home-accent.png"
+        alt=""
+        id="rC1"
+      />
+      <img
+        src="https://assets.thdstatic.com/images/v1/brand-logos/legend-force.png"
+        alt=""
+        id="rC2"
+      />
+    </div>
+    <div id="id501">
+      <p>
+        © 2000-2021 Home Depot Product Authority, LLC. All Rights Reserved.
+        Use of this site is subject to certain <a href="">Terms Of Use</a>.
+      </p>
+      <p>
+        Local store prices may vary from those displayed. Products shown as
+        available are normally stocked but inventory levels cannot be
+        guaranteed
+      </p>
+      <p>
+        For screen reader problems with this website, please call
+        1-800-430-3376 or text 38698 (standard carrier rates apply to texts)
+      </p>
+    </div>
+    <div id="id502">
+      <a href="">Privacy & Security Statement</a>
+      <a href="">Cookie Usage</a>
+      <a href="">My preference center</a>
+      <a href="">California Privacy Rigths & Report</a>
+      <a href="">Do Not Sell My Personal Information</a>
+      <a href="">California Supply Chain Act</a>
+      <a href="">Site Map</a>
+      <a href="">Store Directory</a>
+      <a href="">Mobile Site</a>
+    </div>
+  </div>`;
+};
+
+export default footerFunc;
