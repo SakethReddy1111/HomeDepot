4bdfa69591817b951ebdbe6d7c793d59028ad2c5
saketh reddy <sakethr007@gmail.com>
2022-01-22 11:18:26 +0530

    stair

--- scripts/success.js
+++ scripts/success.js
@@ -1,9 +1,20 @@
 let userDetails = JSON.parse(localStorage.getItem("userDetails"));
 
-let name = document.getElementById("name");
+let username = document.getElementById("name");
+username.textContent = userDetails.firstname + " " + userDetails.lastname;
 
 let address = document.getElementById("address");
+address.textContent =
+  userDetails.hno +
+  ", " +
+  userDetails.town +
+  " ," +
+  userDetails.country +
+  " ," +
+  userDetails.pin;
 
 let number = document.getElementById("number");
+number.textContent = userDetails.mobile;
 
 let payable = document.getElementById("payable");
+payable.textContent = "$" + userDetails.toPay;
